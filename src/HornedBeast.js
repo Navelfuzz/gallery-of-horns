14653b3f222c5944d8a6c7c7adb32f2013652ae5
Jonathon Stillson <jcstillson77@gmail.com>
2023-06-16 07:54:28 -0400

    finished lab

---
+++ src/HornedBeast.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class HornedBeast extends React.Component {
+    render(){
+        console.log(this.props);
+        return(
+            <>
+                <article>
+                    <h2>{this.props.name}</h2>
+                    <img src={this.props.image_url} alt={this.props.description} title={this.props.title}/>
+                </article>
+            </>
+        )
+    }
+}
+export default HornedBeast;
